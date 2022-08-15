c6b1d3016f86fbd607ba7ced01eb07449769ba73
David Raitzyk <dnraitzyk@verizon.net>
2022-08-14 22:30:16 -0400

    trying to resolve app module probs

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -2,4 +2,4 @@ from app import flaskapp as application
 import os
 print("python path ", os.environ['PYTHONPATH'])
 if __name__ == "__main__":
-    application.run(debug=False)
+    application.run(debug=True)
