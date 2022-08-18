a2109325a2c8c492de86dbaf06004650ccee81d1
David Raitzyk <dnraitzyk@verizon.net>
2022-08-17 20:23:50 -0400

    so close to getting heroku, working frontend

--- app.py
+++ app.py
@@ -42,6 +42,7 @@ else:
     template_dir = os.path.join(template_dir, 'frontend')
     template_dir = os.path.join(template_dir, 'build')
 
+print(os.listdir(template_dir))
 
 # template_dir = os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
 
