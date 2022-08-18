3bd283325bf6286a8b938a1521b99eeaac5d9079
David Raitzyk <dnraitzyk@verizon.net>
2022-08-17 22:35:27 -0400

    heroku working locally now, but not online

--- wsgi.py
+++ wsgi.py
@@ -2,4 +2,6 @@ from app import flaskapp as application
 import os
 # print("python path ", os.environ['PYTHONPATH'])
 if __name__ == "__main__":
-    application.run(debug=True)
+     port = int(os.environ.get("PORT", 5000))
+    # app.run(host='0.0.0.0', port=port)
+    application.run(host='0.0.0.0', port=port)
