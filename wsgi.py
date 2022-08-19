de625c26afbdfdeec28abefa89efeaea141c1db3
David Raitzyk <dnraitzyk@verizon.net>
2022-08-18 20:06:26 -0400

    resolved pip

--- wsgi.py
+++ wsgi.py
@@ -2,6 +2,7 @@ from app import flaskapp as application
 import os
 # print("python path ", os.environ['PYTHONPATH'])
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT"))
+    # port = int(os.environ.get("PORT"))
     # app.run(host='0.0.0.0', port=port)
-    application.run(host='0.0.0.0', port=port)
+    # application.run(host='0.0.0.0', port=port)
+    application.run()
