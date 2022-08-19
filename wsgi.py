ef9e92f1aa643e69331dcafa2139db644ca9d6f5
David Raitzyk <dnraitzyk@verizon.net>
2022-08-18 20:34:32 -0400

    trying to fix flask routes heroku

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,14 @@
 from app import flaskapp as application
 import os
 # print("python path ", os.environ['PYTHONPATH'])
+from dotenv import load_dotenv
+
+load_dotenv()
+isheroku = os.environ.get('ISHEROKU')
 if __name__ == "__main__":
-    # port = int(os.environ.get("PORT"))
+
+    port = int(os.environ.get('PORT', 5000))
+
     # app.run(host='0.0.0.0', port=port)
-    # application.run(host='0.0.0.0', port=port)
-    application.run()
+    application.run(host='0.0.0.0', port=port)
+    # application.run()
