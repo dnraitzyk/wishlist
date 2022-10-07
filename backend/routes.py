1da433c9e03fd5aafd3c896b2d8a084fd5bc418c
David Raitzyk <dnraitzyk@verizon.net>
2022-10-06 22:40:21 -0400

    test procfile, made import changes in routes

--- backend/routes.py
+++ backend/routes.py
@@ -2,7 +2,7 @@ import time
 import argparse
 # from tkinter import W
 # from backend import *
-from . import Wishlist
+import Wishlist
 # from backend.User import User
 from .external import getAllExternal
 import logging
